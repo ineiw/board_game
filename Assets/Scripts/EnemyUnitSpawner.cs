5884fe82c45c14d978393a63ac62a718ed3f355e
ineiw <75738349+ineiw@users.noreply.github.com>
2022-11-23 13:54:55 +0900

    feat: 애너미 유닛 추가및 랜덤 생성 기능 추가
    enemyunit 을 정해진 확률로 랜덤하게 생성
    enemyunit 총 3개

--- Assets/Scripts/EnemyUnitSpawner.cs
+++ Assets/Scripts/EnemyUnitSpawner.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyUnitSpawner : MonoBehaviour
 {
     const float SPAWN_INTERVAL = 2f;
-    public GameObject enemyUnitPrefab = null;
+    const float EPS = 0.3f;
+    public GameObject[] enemyUnitPrefabs = new GameObject[3];
     public float durationTime = SPAWN_INTERVAL;
     // Update is called once per frame
     void Update()
@@ -14,13 +15,25 @@ public class EnemyUnitSpawner : MonoBehaviour
     }
 
     public void SpawnEnemyUnit(int enemyCount){
+
         for(int i=0;i<enemyCount;i++){
             Vector2 spawnPosition = new Vector2(Random.Range(-5f, 5f), Random.Range(-3f, 3f));
-            Instantiate(enemyUnitPrefab, spawnPosition, Quaternion.identity);
+            Instantiate(enemyUnitPrefabs[returnIdx()], spawnPosition, Quaternion.identity);
         }
         Debug.Log("Spawn enemy unit");
     }
 
+    int returnIdx(){
+        float prop = Random.Range(0f,1f);
+        int idx = 0;
+        if(EPS > prop){
+            idx = 1;
+            if(0.5f > Random.Range(0f,1f)){
+                idx = 2;
+            }
+        }
+        return idx;
+    }
     public void SpawnEnemyUnitWhenEnemyUnitIsDead(){
         if(GameObject.FindGameObjectsWithTag("EnemyUnit").Length == 0){
             if(durationTime <= 0){
